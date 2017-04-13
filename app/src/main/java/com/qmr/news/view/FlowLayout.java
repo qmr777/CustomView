5322e954f5978c748670bcc408381f413abcd57c
qmr777 <qmr777@gmail.com>
2017-04-13 16:50:58 +0800

    改bug

--- app/src/main/java/com/qmr/news/view/FlowLayout.java
+++ app/src/main/java/com/qmr/news/view/FlowLayout.java
@@ -119,7 +119,6 @@ public class FlowLayout extends ViewGroup {
                 currentWidth += child.getMeasuredWidth() + lp.rightMargin;
             }
         }
-
     }
 
     private int getWidthWithPadding() {
